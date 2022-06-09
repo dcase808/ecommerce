d763958b4da0f66d109f44db7e4af40ccdeb419b
dcase808 <xmarcinx125@gmail.com>
2022-06-09 23:23:01 +0200

    add finding items by tag

--- back/src/app/api/routers/items.py
+++ back/src/app/api/routers/items.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends
+from re import A
+from fastapi import APIRouter, Depends, Query
 from ..auth import get_current_user
 from ...models import Item
+from beanie.operators import In
 
 router = APIRouter(
     prefix='/items',
@@ -8,5 +10,8 @@ router = APIRouter(
 )
 
 @router.get('', response_model=list[Item])
-async def get_items():
-    return await Item.find().to_list()
+async def get_items(tag: str | None = Query(None)):
+    if tag:
+        return await Item.find(In(Item.tags, [tag])).to_list()
+    else:
+        return await Item.find().to_list()
