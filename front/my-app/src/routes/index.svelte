54de3e9d7371921b75872df98e47c6a57ae9d5c2
Ziomidlo <kziomek33@gmail.com>
2022-06-10 14:15:18 +0200

    delete init app

--- front/my-app/src/routes/index.svelte
+++ front/my-app/src/routes/index.svelte
@@ -1,33 +1,15 @@
-<script context="module">
-	export const prerender = true;
-</script>
 
 <script>
-	import Counter from '$lib/Counter.svelte';
+
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>DShop</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
 </section>
 
 <style>
@@ -39,23 +21,5 @@
 		flex: 1;
 	}
 
-	h1 {
-		width: 100%;
-	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
