ac1858090e11fc524ab3fa38e3443533a940fe42
dcase808 <xmarcinx125@gmail.com>
2022-06-10 19:11:04 +0200

    add upload image endpoint

--- back/src/app/admin/routers/admin_items.py
+++ back/src/app/admin/routers/admin_items.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile, HTTPException, status
 from ..auth import get_current_user
 from ...models import Item, ItemUpdate
+from ...db_connect import bucket
 
 router = APIRouter(
     prefix='/items',
@@ -15,8 +16,17 @@ async def get_items():
 async def add_item(item: Item):
     return await item.insert()
 
-@router.patch('/', response_model=Item, dependencies=[Depends(get_current_user)])
+@router.patch('', response_model=Item, dependencies=[Depends(get_current_user)])
 async def update_item(data: ItemUpdate):
     updated_data = data.dict(exclude_unset=True, exclude={'id'})
     await Item.find_one({'_id': data.id}).update({'$set': updated_data})
     return await Item.find_one({'_id': data.id})
+
+@router.post('/image')
+async def upload_image(file: UploadFile):
+    if file.content_type == 'image/png' or file.content_type == 'image/jpg':
+        content = await file.read()
+        bucket.upload_bytes(content, file.filename, file.content_type)
+        return {'file_url': bucket.get_download_url(file.filename)}
+    else:
+        raise HTTPException(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
--- back/src/app/db_connect.py
+++ back/src/app/db_connect.py
@@ -1,8 +1,17 @@
-from .secrets import CONNECTION_STRING
+from .secrets import CONNECTION_STRING, B2_KEY_ID, B2_APPLICATION_KEY
 from beanie import init_beanie
 import motor.motor_asyncio
 from .models import Item, User, Order, Admin
+from b2sdk.v2 import InMemoryAccountInfo, B2Api
 
 async def init():
     client = motor.motor_asyncio.AsyncIOMotorClient(CONNECTION_STRING)
     await init_beanie(database=client.shopv2, document_models=[User, Item, Order, Admin])
+
+def init_image_storage():
+    info = InMemoryAccountInfo()
+    b2_api = B2Api(info)
+    b2_api.authorize_account('production', B2_KEY_ID, B2_APPLICATION_KEY)
+    return b2_api.get_bucket_by_name('ecommerce-36431')
+
+bucket = init_image_storage()
