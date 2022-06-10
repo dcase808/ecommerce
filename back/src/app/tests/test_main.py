bfe602e9c7bdec8de69e85062c9fbdd07e0e6eff
dcase808 <xmarcinx125@gmail.com>
2022-06-10 13:06:39 +0200

    update tests

--- back/src/app/tests/test_main.py
+++ back/src/app/tests/test_main.py
@@ -4,5 +4,13 @@ from ..main import app
 client = TestClient(app)
 
 def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 404
+
+def test_get_main_api():
     response = client.get("/v1/api/")
-    assert response.status_code == 204
+    assert response.status_code == 404
+
+def test_get_main_admin():
+    response = client.get("/v1/admin/")
+    assert response.status_code == 404
