d30de1682c32a8fb93cdd525bc837b1da9f5782c
Ziomidlo <kziomek33@gmail.com>
2022-06-14 01:44:19 +0200

    change user side

--- front/my-app/src/routes/User.svelte
+++ front/my-app/src/routes/User.svelte
@@ -4,6 +4,7 @@
     import { onMount } from 'svelte';
     import {API_URL} from '$lib/Constans/Constans.svelte'
     import User from '../lib/Components/User.svelte';
+import Orders from '../lib/Components/Orders.svelte';
 
     let loggedIn = false
 
@@ -44,13 +45,25 @@
         if(Cookies.get('jwt-token')) {
             validateAndForward()
             let url = API_URL + '/users/me'
-            console.log(url)
 		    return await fetch(url)
 		    .then(response => response.json())
 		    .then(response => response)
             .catch(e => console.error(e))
         }
     }
+
+    const getOrders = async () => {
+        if(Cookies.get('jwt-token')) {
+            validateAndForward()
+            let url = API_URL + '/orders/'
+		    return fetch(url)
+		    .then(response => response.json())
+		    .then(response => response)
+            .catch(e => console.error(e))
+        }
+    }
+
+    let orders = getOrders()
     let user = getUser()
 </script>
 
@@ -61,6 +74,13 @@
         Loading
     {:then user} 
        <User id={user._id} name={user.name} address={user.address} postal_code={user.postal_code} city={user.city}/>
+    {/await}
+    Zamowienia
+    
+    {#await orders}
+        Loading
+    {:then orders} 
+    <Orders _id={orders._id} item = {[orders.id, orders.quantity]}  price = {orders.price} paid = {orders.paid} /> 
     {/await}
     <button on:click={logout}>Wyloguj</button>   
     {/if}
