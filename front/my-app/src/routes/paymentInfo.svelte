c3d98835d5040264afd126f2041003b465ba626f
Ziomidlo <kziomek33@gmail.com>
2022-06-14 06:17:05 +0200

    create payment info route

---
+++ front/my-app/src/routes/paymentInfo.svelte
@@ -0,0 +1,21 @@
+<script>
+    let status = true
+</script>
+
+<div class="info">
+    {#if status}
+    Pomyślnie wykonano płatność!
+    {:else}
+    Błąd przy wykonywaniu płatności.
+    {/if}
+    <br>
+    Sprawdź status zamówienia w zakładce Twoje konto
+</div>
+
+<style>
+    .info {
+        text-align: center;
+        margin: auto;
+        padding: 20px;
+    }
+</style>
