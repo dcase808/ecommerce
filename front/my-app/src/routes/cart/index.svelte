d6342b198a0607f491d2c358c4884e744f8917b3
dcase808 <xmarcinx125@gmail.com>
2022-06-14 10:05:08 +0200

    finishing touches in cart route

--- front/my-app/src/routes/cart/index.svelte
+++ front/my-app/src/routes/cart/index.svelte
@@ -2,20 +2,62 @@
     import { shopping_cart } from '$lib/stores.js'
     import ItemInCart from '$lib/Components/ItemInCart.svelte';
 
-    let cart = []
+    const removeQuantity = (id) => {
+        for (const obj of $shopping_cart) 
+        {
+            if (obj.item_id === id) 
+            {
 
-    shopping_cart.subscribe(value => {
-        cart = value
-    })
-
+                obj.quantity--;
+                $shopping_cart = $shopping_cart
+                if(obj.quantity < 1)
+                {
+                    $shopping_cart = $shopping_cart.filter(e => {
+                        return e.item_id != id;
+                    })
+                }
+            }
+        }
+        console.log($shopping_cart)
+    }
+    const addQuantity = (id) => {
+        for (const obj of $shopping_cart) 
+        {
+            if (obj.item_id === id) 
+            {
+                obj.quantity++;
+                $shopping_cart = $shopping_cart
+            }
+        }
+    }
+    $: calculateSum = () => {
+        let sum = 0
+        for (const obj of $shopping_cart)
+        {
+            sum = sum + obj.quantity * obj.price
+        }
+        return sum
+    }
+    const payment = () => {
+        console.log('test')
+    }
 
 </script>
 
 <main>
-    {#if cart.length !== 0}
-        {#each cart as item}
-            <ItemInCart id={item.item_id} quantity={item.quantity}/>
+    
+    {#if $shopping_cart.length !== 0}
+        {#each $shopping_cart as item}
+        <section>
+            <div class='item'>
+                <ItemInCart id={item.item_id} quantity={item.quantity}/>
+            </div>
+            <div class='buttons'>
+                <button class='change-quantity' on:click={() => {removeQuantity(item.item_id)}}>-</button><button class='change-quantity' on:click={() => {addQuantity(item.item_id)}}>+</button>
+            </div>
+        </section>
         {/each}
+            <div class='summary'>Suma: {calculateSum().toFixed(2)} PLN <button class='go-to-payment' on:click={payment}>Przejdż do podsumowania</button></div>
         {:else}
             <p>Koszyk jest pusty.</p>
     {/if}
@@ -24,6 +66,34 @@
     main{
         width: 70%;
         margin: auto;
-
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    section {
+        width: 100%;
+        display: flex;
+        align-items: center;
+    }
+    .item{
+        width: 80%;
+    }
+    .buttons{
+        width: 20%;
+        margin-left: 70px;
+        margin-right: -100px;
+    }
+    .change-quantity{
+        width: 50px;
+        height: 50px;
+        border: none;
+    }
+    .go-to-payment {
+        border: none;
+        height: 50px;
+    }
+    .summary {
+        margin-left: auto;
+        margin: 20px 50px 20px auto;
     }
 </style>
